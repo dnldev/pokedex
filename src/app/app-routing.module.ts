9ae56bbb330c2876cbc5d8941d4a6e77cc982aec
danielungersboeck <daniel.ungersboeck@gmail.com>
2023-03-02 17:27:29 +0100

    add router guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import {PokemonListComponent} from "./pokemon-list/pokemon-list.component";
 import {PokemonDetailComponent} from "./pokemon-detail/pokemon-detail.component";
 import {CreateProductPageComponent} from "./create-product-page/create-product-page.component";
 import {ProductListComponent} from "./product-list/product-list.component";
+import {getLocalStorageProducts} from "./types/Product";
 
 const routes: Routes = [
   { path: '', redirectTo: 'pokemon-list', pathMatch: 'full' },
   { path: 'pokemon-list', component: PokemonListComponent },
   { path: 'pokemon-detail', component: PokemonDetailComponent },
   { path: 'create-product', component: CreateProductPageComponent },
-  { path: 'product-list', component: ProductListComponent },
+  { path: 'product-list', component: ProductListComponent, canActivate: [() => getLocalStorageProducts().length > 0] },
 ];
 
 @NgModule({
